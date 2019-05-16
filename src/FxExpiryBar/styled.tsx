65c470a13b138828d5b9c52b4265191f19574cb0
Nikhil <nikhil.bhandari.74@gmail.com>
2019-05-16 10:27:01 +0800

    Reformatted code

--- src/FxExpiryBar/styled.tsx
+++ src/FxExpiryBar/styled.tsx
@@ -39,16 +39,16 @@ export const ProgressBar = styled(Collapsable) <{ percentage: Number }>`
   > div {
     width: ${props => (props.percentage > 0 ? props.percentage : 0) + `%`};
     background-color: ${(props) => {
-      let color;
-      if (props.percentage > 66) {
-        color = 'red';
-      } else if (props.percentage > 33) {
-        color = 'orange';
-      } else {
-        color = 'green';
-      }
-      return color;
-    }};
+    let color;
+    if (props.percentage > 66) {
+      color = 'red';
+    } else if (props.percentage > 33) {
+      color = 'orange';
+    } else {
+      color = 'green';
+    }
+    return color;
+  }};
     max-width: 100%;
     transition: width 1s linear, background-color  1s linear;
     height: 10px;
